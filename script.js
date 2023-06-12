947af8fa2e65188bc6fba3ba0bc5bd63e9658d66
Hamseapdiasiis <hamse906@gmail.com>
2023-06-12 09:35:11 +0300

    Exercise done

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Good Luck 💪🏾
+function mostExpensive(item){
+
+    let expensive = 0
+
+
+    for(let i=0; i<item.length; i++){
+        if(item.price > expensive){
+            expensive = item.price
+            console.log(expensive)
+        }
+    }
+    
+}
+
+mostExpensive(
+    [{name: "Daimond Earrings", price: 908},
+    {name: "Gold watch", price: 250},
+    {name: "pearl Necklace", price:4650}]
+)
+
+mostExpensive(
+    [{name: "Silver bracelet", price: 280},
+    {name: "Gemstone Earrings", price: 180},
+    {name: "Daimond ring", price: 3500}]
+)
